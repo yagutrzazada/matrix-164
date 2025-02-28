b3e0d52e500bd6085b00f63a1afdd2a8655c88ad
yagutrzazada <yaqutrzazade@gmail.com>
2025-03-01 01:21:02 +0400

    This commit is for Student Service and StduentInformation

---
+++ src/StudentInformation.java
@@ -0,0 +1,38 @@
+public class StudentInformation {
+    private int Id;
+    private String name;
+    private String surName;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+}
